aad3af2eb5b103584abf07d4d5a5c9ed142b0048
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-18 11:48:41 +0100

    convert string to vector of chars & index over chars

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,17 @@ fn main() {
     io::stdin().read_line(&mut user_input)
         .expect("Failed to read line");
     let user_input = user_input.trim_end_matches("\n");
+
+    let chars: Vec<char> = user_input.chars().collect();
+
+    let length = user_input.len();
+
+    for i in 0..(length / 2) {
+        if chars[i] != chars[length-1-i] {
+            println!("{} is not a palindrome.", user_input);
+            return;
+        }
+    }
+
+    println!("{} is a palindrome!", user_input);
 }

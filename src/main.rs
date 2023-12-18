dc4a64c9cb0323425b3d30675f1a6aca66d7969c
kjubik <wojt.kubicki@gmail.com>
2023-12-18 11:30:32 +0100

    reading & formatting user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io;
+
 
 fn main() {
-    println!("Hello, World!");
+    println!("Please, enter in a word.");
+
+    let mut user_input = String::new();
+
+    io::stdin().read_line(&mut user_input)
+        .expect("Failed to read line");
+    let user_input = user_input.trim_end_matches("\n");
 }

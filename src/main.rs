105388dda553a2c26e41f48c81e1825e9a4f4ee5
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-19 02:16:50 +0100

    revert main to barebones file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,4 @@
-use std::io;
-
 
 fn main() {
-    println!("Please, enter in a word.");
-
-    let mut user_input = String::new();
-
-    io::stdin().read_line(&mut user_input)
-        .expect("Failed to read line");
-    let user_input = user_input.trim_end_matches("\n");
-
-    let chars: Vec<char> = user_input.chars().collect();
-
-    let length = user_input.len();
-
-    for i in 0..(length / 2) {
-        if chars[i] != chars[length-1-i] {
-            println!("{} is not a palindrome.", user_input);
-            return;
-        }
-    }
-
-    println!("{} is a palindrome!", user_input);
+    println!("Hello, World!");
 }

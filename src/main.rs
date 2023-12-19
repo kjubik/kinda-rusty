843b42c88a6111ab7212f84c3ad75fb85ca1d8ba
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-19 02:23:18 +0100

    get started

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use std::io;
+
+
+fn isEvenIterate(number) {
+    if number % 2 == 0 { return true }
+    else { return false }
+}
+
+fn isEvenRecurent(number) {
+    while number > 1 {
+        if number == 0 { return true }
+        if number == 1 { return false }
+        isEvenRecurent(number - 2)
+    }
+}
 
 fn main() {
-    println!("Hello, World!");
+    println!("Enter in a number");
+    
 }
